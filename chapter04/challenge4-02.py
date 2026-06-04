1773d6a822b9d424be36d8ba1f7f5d9689d1e754
itc-n26007 <n26007@std.it-college.ac.jp>
2026-06-04 06:29:05 +0000

    P69 第4章 チャレンジ問題 終了

---
+++ chapter04/challenge4-02.py
@@ -0,0 +1,4 @@
+def show_text(text):
+    print(text)
+
+show_text("こんにちは")
---
+++ chapter04/challenge4-03.py
@@ -0,0 +1,5 @@
+def add_mult(a,b,c,x=10,z=20):
+    return a + b + c * x * z
+
+result = add_mult(10, 15, 25)
+print(result)
